c9adea3d4ceb9e695c30ffb49b0135c7caa8971c
gourisnair <gouri.gopan@gmail.com>
2018-10-21 23:03:57 +0530

    To implement binary max heap using array

---
+++ BinaryMaxHeap.java
@@ -0,0 +1,88 @@
+class Heap {
+  int size;
+  int a[];
+  Heap(int size) {
+    this.size = size;
+    a = new int[size];
+  }
+
+  public int leftChild(int pos) {
+    return 2*pos+1;
+  }
+
+  public int rightChild(int pos) {
+    return 2*pos+2;
+  }
+
+  public int parent(int pos) {
+    return pos/2;
+  }
+
+  public void swap(int papos, int chpos) {
+    int temp = a[papos];
+    a[papos] = a[chpos];
+    a[chpos] = temp;
+  }
+
+  public void insertParent(int data) {
+    a[0] = data;
+  }
+
+  /*public void insertLeft(int data, int pos) {
+    if(a[parent(pos)] > data) {
+      a[2*pos+1] = data;
+    } else {
+      a[2*pos+1] = data;
+      swap(pos, 2*pos+1);
+    }
+  }
+
+  public void insertRight(int data, int pos) {
+    if(a[parent(pos)] > data) {
+      a[2*pos+2] = data;
+    } else {
+      a[2*pos+2] = data;
+      swap(pos, 2*pos+2);
+    }
+  }*/
+
+public void insertLeft(int data, int pos) {
+  if(a[parent(2*pos+1)] > data) {
+    a[2*pos+1] = data;
+  } else {
+    insertLeft(data, parent(pos));
+    swap(pos, 2*pos+1);
+  }
+}
+
+public void insertRight(int data, int pos) {
+  if(a[parent(2*pos+2)] > data) {
+    a[2*pos+2] = data;
+  } else {
+    insertRight(data, parent(pos));
+    swap(pos, 2*pos+2);
+  }
+}
+
+  public void printLevelOrder() {
+    for(int i = 0; i < 7; i++) {
+      System.out.println(a[i]);
+    }
+  }
+
+}
+
+
+public class BinaryMaxHeap {
+  public static void main(String[] args) {
+    Heap h = new Heap(10);
+    h.insertParent(50);
+    h.insertLeft(7, 0);
+    h.insertRight(40, 0);
+    h.insertLeft(10, 1);
+    h.insertRight(25, 1);
+    h.insertRight(30, 2);
+    h.printLevelOrder();
+
+  }
+}
